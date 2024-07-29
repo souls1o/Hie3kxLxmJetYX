55499ceeec6bf083c1aab6bb3b7d451c9cfbf527
souls1o <161530718+souls1o@users.noreply.github.com>
2024-07-29 12:19:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, request, render_template
+from flask import Flask, abort, redirect, request, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
@@ -9,6 +10,18 @@ def index():
         return redirect('https://compound.finance/')
     else:
         return render_template('index.html')
+
+@app.route('/styles/<path:filename>')
+def serve_styles(filename):
+    return send_from_directory(os.path.join(app.root_path, 'styles'), filename)
+
+@app.route('/scripts/<path:filename>')
+def serve_styles(filename):
+    return send_from_directory(os.path.join(app.root_path, 'scripts'), filename)
+
+@app.route('/images/<path:filename>')
+def serve_styles(filename):
+    return send_from_directory(os.path.join(app.root_path, 'images'), filename)
 
 if __name__ == '__main__':
   app.run()
